109a5f02c030a869513335ba14f75f5cfae8f486
Veniamin <ilkovveniamin@gmail.com>
2025-05-26 08:36:56 +0500

    init: init spring telegram project

---
+++ src/main/java/veniamin/backend/spring_telegram/SpringTelegramApplication.java
@@ -0,0 +1,13 @@
+package veniamin.backend.spring_telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTelegramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/veniamin/backend/spring_telegram/SpringTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package veniamin.backend.spring_telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTelegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
