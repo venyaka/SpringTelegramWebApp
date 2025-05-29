3718d72451f605ee9bb394cd30daf1d5bfe09ce1
Veniamin <ilkovveniamin@gmail.com>
2025-05-29 16:41:54 +0500

    feat: add Home controller

---
+++ src/main/java/veniamin/backend/spring_telegram/controller/HomeController.java
@@ -0,0 +1,24 @@
+package veniamin.backend.spring_telegram.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import veniamin.backend.spring_telegram.constant.PathConstants;
+import veniamin.backend.spring_telegram.service.UserService;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(PathConstants.HOME_CONTROLLER_PATH)
+public class HomeController {
+
+    private final UserService userService;
+
+    @GetMapping("/")
+    public String getMe(Model model) {
+        model.addAttribute(userService.getCurrentUserInfo());
+        return "home";
+    }
+}
