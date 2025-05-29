86937042983a5dcb769766a3e21ae08bf6788118
Veniamin <ilkovveniamin@gmail.com>
2025-05-29 16:42:20 +0500

    feat: add url paths

---
+++ src/main/java/veniamin/backend/spring_telegram/constant/PathConstants.java
@@ -0,0 +1,7 @@
+package veniamin.backend.spring_telegram.constant;
+
+public class PathConstants {
+
+    public static final String AUTHORIZE_CONTROLLER_PATH = "/authorize";
+    public static final String HOME_CONTROLLER_PATH = "";
+}
