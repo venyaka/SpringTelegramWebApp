68ca5fcc7fe3cb7c7f677bb6d82d90bf09141128
Veniamin <ilkovveniamin@gmail.com>
2025-05-30 13:14:39 +0500

    refactor: refactor.

--- src/main/java/veniamin/backend/spring_telegram/controller/HomeController.java
+++ src/main/java/veniamin/backend/spring_telegram/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import veniamin.backend.spring_telegram.constant.PathConstants;
 import veniamin.backend.spring_telegram.service.UserService;
+import veniamin.backend.spring_telegram.util.NgrokFetcher;
 
 
 @Controller
